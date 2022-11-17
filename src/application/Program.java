eb1fcd0b1f78f5fdfd9ad4876b810c3b54d330bb
Paulo Dobrychtop <natodobry2@gmail.com>
2022-11-17 16:12:02 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -9,6 +12,8 @@ public class Program {
 
         Department dp = new Department(1, "Books");
 
-        System.out.println(dp);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+
+        System.out.println(seller);
     }
 }
